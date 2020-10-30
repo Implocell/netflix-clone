b528138e71de4c8af6aaf19796809035ff040ae5
Gran <thomaskgran@gmail.com>
2020-10-30 16:50:41 +0100

    landing except header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home, Browse, Signin, Signup } from "./pages";
 
-import Jumbotron from "./components/jumbotron";
-import jumboData from "./fixtures/jumbo";
+import * as ROUTES from "./constants/routes";
 
 export default function App() {
 	return (
-		<Jumbotron.Container>
-			{jumboData.map((item) => (
-				<Jumbotron key={item.id} direction={item.direction}>
-					<Jumbotron.Pane>
-						<Jumbotron.Title>{item.title}</Jumbotron.Title>
-						<Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-					</Jumbotron.Pane>
-					<Jumbotron.Pane>
-						<Jumbotron.Image src={item.image} alt={item.alt} />
-					</Jumbotron.Pane>
-				</Jumbotron>
-			))}
-		</Jumbotron.Container>
+		<Router>
+			<Route exact path={ROUTES.HOME} component={Home} />
+			<Route exact path={ROUTES.BROWSE} component={Browse} />
+			<Route exact path={ROUTES.SIGNUP} component={Signup} />
+			<Route exact path={ROUTES.SIGNIN} component={Signin} />
+		</Router>
 	);
 }
